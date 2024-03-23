4818f5f4f44a6899f1b1f935a5ba1b5d927c4f45
Jimmy00666 <jimilito06@gmail.com>
2024-03-23 19:47:35 +0200

    first commit

--- first.py
+++ first.py
@@ -6,7 +6,7 @@ def sum(a, b, c):
 
 a = int(input('Enter 1st number: '))
 b = int(input('Enter 2nd number: '))
-c = int(input('Enter 23nd number: '))
+c = int(input('Enter 23nd number:  '))
 
 print(f'Sum of {a} and {b} and {c}is {sum(a, b, c)}')
 
