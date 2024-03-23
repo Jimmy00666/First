bb59f430289c33ce1eaad8b51fd871811a412d06
Jimmy00666 <jimilito06@gmail.com>
2024-03-23 19:48:45 +0200

    second commit

--- first.py
+++ first.py
@@ -8,5 +8,5 @@ a = int(input('Enter 1st number: '))
 b = int(input('Enter 2nd number: '))
 c = int(input('Enter 23nd number:  '))
 
-print(f'Sum of {a} and {b} and {c}is {sum(a, b, c)}')
+print(f'Sum of {a} and {b} and {c}is {sum(a, b, c)} ')
 
